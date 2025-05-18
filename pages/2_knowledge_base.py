955361e196d12f1420ed21f53d8520e505e37dfd
YuanDa <2117543204@qq.com>
2025-05-18 23:41:47 +0800

    稍微完善了前端结构

--- pages/2_knowledge_base.py
+++
@@ -1,8 +0,0 @@
-# 该页面任务：
-
-import streamlit as st
-st.title("知识库管理界面")
-if st.session_state.logined==False:
-    st.error("请先登录!")
-    st.stop()
-uploaded_file = st.file_uploader("上传一个文件", type=["pdf","docx","pptx","txt","md"])
