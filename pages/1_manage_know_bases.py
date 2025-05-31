0dbfe904e16ba392361ea82196529f17dfc4dc68
YuanDa <2117543204@qq.com>
2025-05-31 16:37:24 +0800

    14周更新

--- pages/1_manage_know_bases.py
+++ pages/1_manage_know_bases.py
@@ -5,12 +5,10 @@ st.title("知识库管理界面")
 if "logined_user" not  in st.session_state:
     st.error("请先登录!")
     st.stop()
-
-if "pre_kb" not in st.session_state:
-    # 默认选中一个知识库
-    st.session_state.pre_kb="user2117543200@qq.com_kb0"
-
-# 1、显示知识库列表（要求选中一个知识库后才能进行QA）
-#  知识库(collection_name)举例："user2117543200@qq.com_kb0"
-
-# 2、显示知识库文件详情
+else:
+    # 1、显示所有知识库（要求选中一个知识库后才能进行QA）
+    #  知识库(collection_name)举例："user2117543200@qq.com_kb0"
+    # 2、新建知识库
+    # 3、删除知识库
+    # 4、查看与编辑知识库
+    pass
